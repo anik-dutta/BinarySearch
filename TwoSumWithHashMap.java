481d8136f355abe4dbbf7d5fe914fd472239400b
Anik <anikdutta025@gmail.com>
2021-03-03 23:40:15 +0530

    Update TwoSumWithHashMap.java

--- TwoSumWithHashMap.java
+++ TwoSumWithHashMap.java
@@ -15,10 +15,10 @@ public class TwoSumWithHashMap
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the number of elements u want to store: ");
+		System.out.print("Enter the number of elements to store: ");
 		int n = sc.nextInt();
 		int nums[] = new int[n];
-		System.out.println("Enter the elements of the array: "); 
+		System.out.println("Enter the elements: "); 
 		for (int i = 0; i < n; i ++)
 		{
 			nums[i] = sc.nextInt();
